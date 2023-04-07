e787e3a8f021038208512442da64708d94124d66
Matheus Barretto <78603546+mattd-silva22@users.noreply.github.com>
2023-04-07 00:39:11 -0300

    base sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Flex } from '@chakra-ui/react'
+import SideBar from '@/components/sideBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,7 @@ export default function Home() {
       </Head>
       
       <Flex>
-        <h1>Ola amir</h1>
+        <SideBar/>
       </Flex>
     </>
   )
