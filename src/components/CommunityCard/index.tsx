3da0dd35ba480dc2802bbbbf1e9b308fc7a7d13a
roymas100 <amir.dantas@hotmail.com>
2023-04-07 20:42:28 -0300

    add/canvas3d

--- src/components/CommunityCard/index.tsx
+++ src/components/CommunityCard/index.tsx
@@ -1,105 +1,104 @@
-import { Flex , Image, Text} from '@chakra-ui/react'
-import { Star, Users} from 'react-feather'
-import React from 'react'
+import {
+  Flex,
+  Image,
+  Tag,
+  TagLabel,
+  TagLeftIcon,
+  Text,
+} from "@chakra-ui/react";
+import { Star, Users } from "react-feather";
+import React from "react";
 
 type CommunityCardProps = {
-  title : string
-  description : string
-  imgUrl?: string
-  type: string
-  color? : string
-}
+  title: string;
+  description: string;
+  imgUrl?: string;
+  type: string;
+  color?: string;
+};
 
-export default function CommunityCard(props:CommunityCardProps) {
-
-  const {title , description , imgUrl, color, type} = props
+export default function CommunityCard(props: CommunityCardProps) {
+  const { title, description, imgUrl, color, type } = props;
   return (
     <Flex
       className={"card-community"}
       flexDir="column"
       alignItems={"center"}
       justifyContent="center"
-      boxShadow='xl'
+      boxShadow="xl"
       w="470px"
       h="317px"
       minW="470px"
       borderRadius="8px"
-      color= "#2D2D2D"
-      
+      color="#2D2D2D"
     >
       <Flex
         w="100%"
         height="156px"
-        background={imgUrl? `url(${imgUrl})` : ""}
+        background={imgUrl ? `url(${imgUrl})` : ""}
         bgColor={color}
         backgroundSize="cover"
         borderTopLeftRadius={"8px"}
         borderTopRightRadius={"8px"}
         position="relative"
-        
-
       >
-        <Flex
-        position="absolute"
-        bottom="10px"
-        right="24px"
-        bgColor="#fff"
-        padding="4px 8px"
-        borderRadius="8px"
-        gap={1}
-        alignItems="center"
-        justifyContent="center"
-        
-      > 
-      {type === "public"? (
-
-      <>
-          <Users
-          width={16}
-          height={16}
-
-          />
-        <Text
-          fontSize="14px"
-          lineHeight="15px"
-          fontWeight="600"
+        <Tag position="absolute" bottom="10px" right="24px">
+          {type === "public" ? (
+            <>
+              {" "}
+              <TagLeftIcon boxSize="16px" as={Users} />
+              <TagLabel fontSize="14px" lineHeight="15px" fontWeight="600">
+                Pública
+              </TagLabel>{" "}
+            </>
+          ) : (
+            <>
+              <TagLeftIcon boxSize="16px" fill="#DAA520" as={Star} />
+              <TagLabel
+                fontSize="14px"
+                lineHeight="15px"
+                fontWeight="600"
+                color="#DAA520"
+              >
+                Premium
+              </TagLabel>{" "}
+            </>
+          )}
+        </Tag>
+        {/* <Flex
+          position="absolute"
+          bottom="10px"
+          right="24px"
+          bgColor="#fff"
+          padding="4px 8px"
+          borderRadius="8px"
+          gap={1}
+          alignItems="center"
+          justifyContent="center"
         >
-          
-          Pública
-        
-        </Text>
-      </>
-
-      ) : (
-
-        <>
-          <Star
-          width={16}
-          height={16}
-          fill="#DAA520"
-
-          />
-        <Text
-          fontSize="14px"
-          lineHeight="15px"
-          fontWeight="600"
-          color="#DAA520"
-
-        >
-          
-          Premium
-        
-        </Text>
-      </>
-
-      )}
-        
-        
-      </Flex>
+          {type === "public" ? (
+            <>
+              <Users width={16} height={16} />
+              <Text fontSize="14px" lineHeight="15px" fontWeight="600">
+                Pública
+              </Text>
+            </>
+          ) : (
+            <>
+              <Star width={16} height={16} fill="#DAA520" />
+              <Text
+                fontSize="14px"
+                lineHeight="15px"
+                fontWeight="600"
+                color="#DAA520"
+              >
+                Premium
+              </Text>
+            </>
+          )}
+        </Flex> */}
       </Flex>
 
-      
-      
       <Flex
         w="100%"
         flexDir="column"
@@ -107,24 +106,16 @@ export default function CommunityCard(props:CommunityCardProps) {
         justifyContent={"flex-start"}
         padding="24px"
         gap={"8px"}
-        color= "#2D2D2D"
+        color="#2D2D2D"
         minH="160px"
       >
-        <Text
-          fontSize="20px"
-          lineHeight="30px"
-          fontWeight={700}
-        >
+        <Text fontSize="20px" lineHeight="30px" fontWeight={700}>
           {title}
         </Text>
-        <Text
-          fontSize="16px"
-          lineHeight="24px"
-          fontWeight={400}
-        >
+        <Text fontSize="16px" lineHeight="24px" fontWeight={400}>
           {description}
         </Text>
       </Flex>
     </Flex>
-  )
+  );
 }
--- src/pages/landing/[communityId].tsx
+++ src/pages/landing/[communityId].tsx
@@ -1,8 +1,17 @@
 import SideBar from "@/components/sideBar";
-import { Button, Flex, Image, Text } from "@chakra-ui/react";
+import {
+  Badge,
+  Button,
+  Flex,
+  Image,
+  Tag,
+  TagLabel,
+  TagLeftIcon,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
-import { UserPlus } from "react-feather";
-import { Application } from "@splinetool/runtime";
+import { UserPlus, Users } from "react-feather";
+import Spline from "@splinetool/react-spline";
 
 const depositions = [
   {
@@ -13,18 +22,33 @@ const depositions = [
     image:
       "https://aniyuki.com/wp-content/uploads/2022/05/aniyuki-anime-avatar-black-and-white-35.jpg",
   },
+  {
+    deposition:
+      "“Antes da Cuidadoso eu tinha dificuldade de encontrar os meus clientes e ser remunerado pelos meus conhecimentos. Agora, ensino e sou remunerado por isso.”",
+    name: "Lucas Ribeiro",
+    role: "Enfermeiro",
+    image:
+      "https://aniyuki.com/wp-content/uploads/2022/05/aniyuki-anime-avatar-black-and-white-35.jpg",
+  },
+  {
+    deposition:
+      "“Sempre tive o interesse em descobrir uma plataforma como essa. Aprendi horrores com os cursos e amei o professores. Super interessante e divertido!!!",
+    name: "Lurdes Sampaio",
+    role: "Aposentada",
+    image:
+      "https://aniyuki.com/wp-content/uploads/2022/05/aniyuki-anime-avatar-black-and-white-35.jpg",
+  },
+  {
+    deposition:
+      "“A Cuidadoso salvou a minha vida várias vezes. A minha mãe adorou os cursos e os encontros do Conecdosos. MUITO SATISFEITA!!!” ",
+    name: "Letícia Ristghneer",
+    role: "Designer de moda",
+    image:
+      "https://aniyuki.com/wp-content/uploads/2022/05/aniyuki-anime-avatar-black-and-white-35.jpg",
+  },
 ];
 
 const LandingPage: React.FC = () => {
-  //   useLayoutEffect(() => {
-  //     const canvas = document.getElementById("canvas3d") as HTMLCanvasElement;
-  //     if (canvas) {
-  //       console.log(canvas);
-  //       const app = new Application(canvas);
-  //       app.load("https://prod.spline.design/w5kNHPJycV26nkPE/scene.splinecode");
-  //     }
-  //   }, []);
-
   return (
     <Flex>
       <SideBar />
@@ -38,7 +62,7 @@ const LandingPage: React.FC = () => {
         <Flex
           backgroundColor="black"
           width="100%"
-          height="276px"
+          height="300px"
           position="absolute"
           top={0}
           zIndex={-1}
@@ -46,100 +70,148 @@ const LandingPage: React.FC = () => {
 
         <Flex
           width="100%"
-          padding="140px"
+          padding="180px"
           paddingLeft="12%"
           paddingRight="12%"
           flexDir="column"
           gap="24px"
         >
-          <Flex mb="24px">
-            <Flex
-              justifyContent="space-between"
-              alignItems="flex-end"
-              width="100%"
-            >
-              <Flex gap="16px">
-                <Flex
-                  width="158px"
-                  height="158px"
-                  borderRadius="4px"
-                  alignItems="center"
-                  justifyContent="center"
-                  backgroundColor="white"
-                  padding="6px"
-                  boxShadow="0px 0px 10px rgba(0, 0, 0, 0.25)"
-                >
-                  <Image
-                    alt="LOGO"
-                    src="https://aniyuki.com/wp-content/uploads/2022/05/aniyuki-anime-avatar-black-and-white-35.jpg"
-                  />
-                </Flex>
-                <Text fontWeight={600} fontSize="32px" color="white">
-                  Nome da Comunidade
-                </Text>
+          <Header />
+
+          <Flex gap="24px" flexDir="column">
+            <Flex backgroundImage="url('/bgpurple.svg')" width="100%">
+              <Spline
+                scene="https://prod.spline.design/VIWFqzpSSvDvM8QR/scene.splinecode"
+                style={{ height: "516px" }}
+              />
+            </Flex>
+
+            <Flex gap="24px">
+              <Flex width="100px" height="100px" backgroundColor="red">
+                <Flex></Flex>
+                <Flex></Flex>
               </Flex>
-              <Button
-                backgroundColor="white"
-                boxShadow="0px 3.47992px 16.5296px rgba(0, 0, 0, 0.25);"
-                leftIcon={<UserPlus />}
-                size="lg"
-              >
-                Ingressar na Comunidade
-              </Button>
+              <Flex></Flex>
             </Flex>
           </Flex>
 
-          <Flex flexDir="column" gap="24px" alignItems="center">
-            <Text
-              fontWeight={600}
-              fontSize="32px"
-              textAlign="center"
-              color="#031432"
-            >
-              Depoimentos dos usuários
-            </Text>
-            <Text
-              color="#6C87AE"
-              font-weight={400}
-              fontSize="16px"
-              maxW="600px"
-              textAlign="center"
-            >
-              Alguns depoimentos de pessoas que usam diariamente a plataforma da
-              Cuidadoso, seja ela um cuidador, um usuário assíduo ou um
-              professor da Cuidadoso Cursos.
-            </Text>
+          <Depoimentos />
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
 
-            <Flex gap="72px" flexWrap="wrap" w="100%">
-              {depositions.map((deposition) => (
-                <Flex
-                  key={deposition.deposition}
-                  padding="32px"
-                  maxW="530px"
-                  boxShadow="0px 24px 32px -3px rgba(3, 9, 50, 0.04)"
-                  borderRadius={32}
-                  flexDir="column"
-                  gap="32px"
-                >
-                  <Text fontStyle="Italic">{deposition.deposition}</Text>
-                  <Flex gap="16px">
-                    <Image
-                      src={deposition.image}
-                      alt={deposition.name}
-                      borderRadius="100%"
-                      width="60px"
-                      height="60px"
-                    />
-                    <Flex flexDir="column" gap="8px">
-                      <Text>{deposition.name}</Text>
-                      <Text>{deposition.role}</Text>
-                    </Flex>
-                  </Flex>
-                </Flex>
-              ))}
+const Header = () => {
+  return (
+    <Flex mb="24px">
+      <Flex justifyContent="space-between" alignItems="flex-end" width="100%">
+        <Flex gap="16px">
+          <Flex
+            width="158px"
+            height="158px"
+            borderRadius="4px"
+            alignItems="center"
+            justifyContent="center"
+            backgroundColor="white"
+            padding="6px"
+            boxShadow="0px 0px 10px rgba(0, 0, 0, 0.25)"
+          >
+            <Image
+              alt="LOGO"
+              src="https://aniyuki.com/wp-content/uploads/2022/05/aniyuki-anime-avatar-black-and-white-35.jpg"
+            />
+          </Flex>
+          <Flex flexDir="column" justifyContent="space-between">
+            <Flex flexDir="column" gap="8px" alignItems="flex-start">
+              <Text fontWeight={600} fontSize="32px" color="white">
+                Nome da Comunidade
+              </Text>
+
+              <Tag>
+                <TagLeftIcon boxSize="16px" as={Users} />
+                <TagLabel fontSize="14px" lineHeight="15px" fontWeight="600">
+                  Pública
+                </TagLabel>
+              </Tag>
+            </Flex>
+
+            <Flex gap="4px">
+              <Badge colorScheme="purple">DESIGN</Badge>
             </Flex>
           </Flex>
         </Flex>
+        <Button
+          marginBottom="16px"
+          backgroundColor="white"
+          boxShadow="0px 3.47992px 16.5296px rgba(0, 0, 0, 0.25);"
+          leftIcon={<UserPlus />}
+          size="lg"
+        >
+          Ingressar na Comunidade
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
+
+const Depoimentos = () => {
+  return (
+    <Flex flexDir="column" gap="24px" alignItems="center">
+      <Text fontWeight={600} fontSize="32px" textAlign="center" color="#031432">
+        Depoimentos dos usuários
+      </Text>
+      <Text
+        color="#6C87AE"
+        fontWeight={400}
+        fontSize="16px"
+        maxW="600px"
+        textAlign="center"
+      >
+        Alguns depoimentos de pessoas que usam diariamente a plataforma da
+        Cuidadoso, seja ela um cuidador, um usuário assíduo ou um professor da
+        Cuidadoso Cursos.
+      </Text>
+
+      <Flex gap="36px" flexWrap="wrap" w="100%" justifyContent="center">
+        {depositions.map((deposition) => (
+          <Flex
+            key={deposition.deposition}
+            padding="32px"
+            maxW="500px"
+            boxShadow="0px 24px 32px -3px rgba(3, 9, 50, 0.04)"
+            borderRadius={32}
+            flexDir="column"
+            gap="32px"
+            justifyContent="space-between"
+          >
+            <Text
+              fontStyle="Italic"
+              fontWeight={300}
+              fontSize="20px"
+              color="#031432"
+            >
+              {deposition.deposition}
+            </Text>
+            <Flex gap="16px">
+              <Image
+                src={deposition.image}
+                alt={deposition.name}
+                borderRadius="100%"
+                width="60px"
+                height="60px"
+              />
+              <Flex flexDir="column" gap="4px">
+                <Text color="#3A8EF6" fontSize="20px" fontWeight={500}>
+                  {deposition.name}
+                </Text>
+                <Text color="#6C87AE" fontWeight={400} fontSize="16px">
+                  {deposition.role}
+                </Text>
+              </Flex>
+            </Flex>
+          </Flex>
+        ))}
       </Flex>
     </Flex>
   );
