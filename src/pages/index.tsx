fd85039cb730fb711e05f7f89667b9deb8febec0
Matheus Barretto <78603546+mattd-silva22@users.noreply.github.com>
2023-04-07 23:03:37 -0300

    add new icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Flex , Text } from "@chakra-ui/react";
+import { Button, Flex , Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
 import SideBar from "@/components/sideBar";
 import HomeHeader from "@/components/HomeHeader";
 import CommunityCard from "@/components/CommunityCard";
@@ -12,9 +12,11 @@ import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
+
 export default function Home() {
   const {communitiesList} = useDb()
   const [sizeWidth, setSizeWidth] = useState("")
+  const { isOpen, onOpen, onClose } = useDisclosure()
   useEffect(()=>{
     if(window) {
       const size = window.innerWidth;
@@ -144,13 +146,41 @@ export default function Home() {
           >
             {communitiesList.map((item , index) => {
               return (
-                <TipsCard
-                  key={index}
-                  title="Explore os módulos da comunidade >"             
-                  color={item.color}
-                />
+                <>
+                  <Flex onClick={onOpen}> 
+                    <TipsCard
+                        key={index}
+                        title="Explore os módulos da comunidade >"             
+                        color={item.color}
+                        
+                      />
+                  
+                  </Flex>
+
+                  
+                    
+                </>
+                
               )
             } )}
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  ola
+                </ModalBody>
+
+                <ModalFooter>
+                  <Button colorScheme='blue' mr={3} onClick={onClose}>
+                    Close
+                  </Button>
+                  <Button variant='ghost'>Secondary Action</Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
             
           </Flex>
         </Flex>
